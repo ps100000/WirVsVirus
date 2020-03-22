302413281eaf34df94ba1c2699c571c76800e4c2
Huakas <lukaspc194@gmail.com>
2020-03-22 11:51:39 +0100

    data class for simple json boolean return created

---
+++ backend/src/main/java/com/wirvsvirus/homealonechallenge/dataClasses/BooleanReturn.java
@@ -0,0 +1,14 @@
+package com.wirvsvirus.homealonechallenge.dataClasses;
+
+public class BooleanReturn {
+
+    Boolean returnBool;
+
+    public BooleanReturn(Boolean returnBool) {
+        this.returnBool = returnBool;
+    }
+
+    public Boolean getReturnBool() {
+        return returnBool;
+    }
+}
